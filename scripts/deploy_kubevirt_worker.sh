6c2c871fb74a763fb9c2f835ec09205603f1cc08
Martin Jackson <martjack@redhat.com>
2022-05-04 17:55:51 -0500

    Update deployer script, having problems with field selector

---
+++ scripts/deploy_kubevirt_worker.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env -S -vvv ansible-playbook
+---
+- name: "Install manifest on AAP controller"
+  become: false
+  connection: local
+  hosts: localhost
+  gather_facts: false
+  vars:
+    kubeconfig: "{{ lookup('env', 'KUBECONFIG') }}"
+    metal_field: 'spec.template.spec.providerSpec.value.instanceType'
+    metal_type: 'c5.metal'
+  tasks:
+    - name: Fetch infrastructure values
+      kubernetes.core.k8s_info:
+        kind: Infrastructure
+        namespace: ''
+        name: cluster
+      register: infra_values
+
+# Do platform specific set facts here
+
+    - name: Check for metal machinesets
+      kubernetes.core.k8s_info:
+        api: "machine.openshift.io/v1beta1"
+        kind: MachineSet
+        namespace: openshift-machine-api
+        field_selectors:
+          - '{{ metal_field }}={{ metal_type }}'
+          #- spec.template.spec.providerSpec.value.instanceType=c5.metal
+      register: metal_machinesets
+
+    - name: Display metal machinesets
+      ansible.builtin.debug:
+        msg: '{{ metal_machinesets }}'
+
+    - name: "End play early if found"
+      meta: end_play
+      when: metal_machinesets.resources | length > 0
+
+    - name: "End play"
+      meta: end_play
+
+    - name: Display infrastructure values
+      ansible.builtin.debug:
+        msg: '{{ infra_values }}'
+
+    - name: Fetch machinesets
+      kubernetes.core.k8s_info:
+        api: "machine.openshift.io/v1beta1"
+        kind: MachineSet
+        namespace: openshift-machine-api
+      register: machine_sets
+
+    - name: Display machineset values
+      ansible.builtin.debug:
+        msg: '{{ machine_sets.resources[0] }}'
+
+    - name: Copy machineset values
+      ansible.builtin.copy:
+        content: '{{ machine_sets.resources[0] | to_nice_yaml }}'
+        dest: /tmp/ms1.yaml
