b81a79337efe6c59c74c468919a2e200805cca4f
Martin Jackson <martjack@redhat.com>
2022-08-26 15:34:00 -0500

    Use configmap for override retrieval

--- scripts/ansible_load_controller.sh
+++ scripts/ansible_load_controller.sh
@@ -4,7 +4,7 @@
   ansible.builtin.import_playbook: ../ansible/ansible_get_credentials.yml
 
 - name: Parse secrets from local values_secret.yaml file
-  ansible.builtin.import_playbook: ../parse_secrets_from_values_secret.yml
+  ansible.builtin.import_playbook: ../ansible/parse_secrets_from_values_secret.yml
 
 - name: Configure AAP instance
   ansible.builtin.import_playbook: ../ansible/ansible_configure_controller.yml
