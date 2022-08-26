1022433c712a531f6e517b9075e2e72f2195ec64
Martin Jackson <martjack@redhat.com>
2022-08-26 09:01:01 -0500

    Refactor secrets loading code

--- scripts/ansible_load_controller.sh
+++ scripts/ansible_load_controller.sh
@@ -3,5 +3,8 @@
 - name: Retrieve AAP credentials
   ansible.builtin.import_playbook: ../ansible/ansible_get_credentials.yml
 
+- name: Parse secrets from local values_secret.yaml file
+  ansible.builtin.import_playbook: ../parse_secrets_from_values_secret.yml
+
 - name: Configure AAP instance
   ansible.builtin.import_playbook: ../ansible/ansible_configure_controller.yml
